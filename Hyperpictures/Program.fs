7ba0cdd725cf4d1df327cd9c4ef5cd3e0e6b1d3e
Einar W. Høst <einarwh@gmail.com>
2025-04-30 14:10:52 +0200

    Update Program.fs

--- Hyperpictures/Program.fs
+++ Hyperpictures/Program.fs
@@ -2,14 +2,20 @@ open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 
+let getHandler (path : string) : string = 
+    sprintf "GET to %s!" path
+
+let postHandler (path : string) : string = 
+    sprintf "POST to %s" path
+
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
     let app = builder.Build()
 
-    app.MapGet("/", Func<string>(fun () -> "Hello World!")) |> ignore
+    app.MapGet("/{**path}", Func<string, string>(getHandler)) |> ignore
+    app.MapPost("/{**path}", Func<string, string>(postHandler)) |> ignore
 
     app.Run()
 
     0 // Exit code
-
