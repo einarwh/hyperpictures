b707b4ee940747d5f9a1b07420ea868974f891a7
Einar W. Høst <einarwh@gmail.com>
2025-04-30 18:08:27 +0200

    fix

--- Hyperpictures/Program.fs
+++ Hyperpictures/Program.fs
@@ -1,21 +1,18 @@
 open System
+open System.Threading.Tasks
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Http
 
-let getHandler (path : string) : string = 
-    sprintf "GET to %s!" path
-
-let postHandler (path : string) : string = 
-    sprintf "POST to %s" path
-
+let getHandler (ctx : HttpContext) : Task = 
+    let routePath = ctx.Request.RouteValues["path"] :?> string
+    let nonNullPath = if routePath = null then "" else routePath
+    ctx.Response.WriteAsync(sprintf "Hello %s" nonNullPath)
+    
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
     let app = builder.Build()
-
-    app.MapGet("/{**path}", Func<string, string>(getHandler)) |> ignore
-    app.MapPost("/{**path}", Func<string, string>(postHandler)) |> ignore
-
+    app.MapGet("/{**path}", Func<HttpContext, Task>(getHandler)) |> ignore
     app.Run()
-
-    0 // Exit code
+    0 
